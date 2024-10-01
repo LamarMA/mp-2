93cdb1c38590736c1ec194166f15f231739e9fd9
lamaralsubhi <Lamarma@bu.edu>
2024-10-01 17:34:21 -0400

    removed unnecassary imports since vercel hated that

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import { Monster } from "../interfaces/Monster";
 import HyruleCompendium from "../components/HyruleCompendium.tsx";
 import styled from "styled-components";
